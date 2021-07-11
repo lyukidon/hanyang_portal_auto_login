9aa50d45569e646251c51ce7ed475a4165a5f8ba
lyukidon <qgx99098@gmail.com>
2021-07-11 20:31:47 +0900

    설문 자동 제출

--- src/Content.js
+++ src/Content.js
@@ -1,5 +1,20 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     //비밀번호 변경
-    const document.querySelector('input#')
+    const pwdUrl="https://portal.hanyang.ac.kr/sso/openPage.do?pgmId=P308193&tk=2cbefdb5b90558e3c72743f63b996894d302feff964db4f6e6cc686fba085da9&returl=https%3A%2F%2Fportal.hanyang.ac.kr%2Fport.do"
+    const btn_cancel = document.querySelector('input#btn_cancel');
     //코로나 자가진단
+    const corona="https://portal.hanyang.ac.kr/openPage.do?pgmId=P320980&tk=daad9bae1afb44e5c91969df5d1031b326e3fea62674ed9f101a5948cd5909f9"
+    const coronaCheck = [...document.querySelectorAll('input')].filter(c => c.id.indexOf('b') !== -1 )
+    const submitCheck = document.querySelector('input#btn_confirm')
+    function click_All_Inputs(inputs){
+        inputs.map(c => c.checked = true)
+        submitCheck.click()
+    }
+    switch (request.action) {
+        case "autoLogin":
+            click_All_Inputs(coronaCheck);
+            break;
+        default:
+            console.log('default')
+    }
 })
--- src/Popup.js
+++ src/Popup.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-    
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+
+        const activeTab = tabs[0];
+        //변수 선언 및 도큐먼트 지정
+        const autoCheckBtn = document.querySelector('button#autoCheck');
+
+        autoCheckBtn.addEventListener("click", ()=>{
+            chrome.tabs.sendMessage(activeTab.id, { action: "autoLogin" })
+        });
+    })
 })
